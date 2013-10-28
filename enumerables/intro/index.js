a9c04972873a2c4dae223dce18ddf19cb27ec98b
michaelpaulson-wf <michael.paulson@webfilings.com>
2013-10-27 19:42:42 -0600

    Working with enumerables.

--- enumerables/intro/index.js
+++ enumerables/intro/index.js
@@ -2,13 +2,17 @@ var $ = require('jquery');
 var Rx = require('rx');
 var Ix = require('ix');
 
+
+// Good news - Enumerables have many of the same methods that Observables do!
+// You don't have to learn 2 new libraries ^^ 
 $(function() {
 
-    var en = Ix.Enumerable.create(function() {
+    // How else to create?  .create method i get errors on moveNext()
+    var en = Ix.Enumerable.repeat(1).select(function() {
         return Math.floor(Math.random() * 100);
     }).filter(function(val) {
         return val > 0.5;
-    }).take(10);
+    }).take(10).getEnumerator();
 
     function jqueryObs($el) {
         return Rx.Observable.create(function(observer) {
@@ -26,6 +30,8 @@ $(function() {
     var $next = $('#next');
     var $output = $('.output');
     var nextObs = jqueryObs($next).subscribe(function() {
-        $output.append(en.next() + '<br/>');
+        if (en.moveNext()) {
+            $output.append(en.getCurrent() + '<br/>');
+        }
     });
 });
